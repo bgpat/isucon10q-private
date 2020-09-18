e074549739c7dc9cb57238ad681ec06dc2d70550
bgpat <bgpat23@gmail.com>
2020-09-19 07:47:20 +0900

    tweak

--- home/isucon/isuumo/webapp/go/estate.go
+++ home/isucon/isuumo/webapp/go/estate.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"sort"
 	"strings"
 	"sync"
@@ -30,56 +29,66 @@ func addEstate(e Estate) {
 func searchEstatesCache(ctx context.Context, doorHeight, doorWidth, rent *Range, features []string, page, perPage int) ([]Estate, int, error) {
 	defer nrsgmt(ctx, "searchEstatesCache").End()
 
-	estates := make([]Estate, 0)
+	all := make([]Estate, 0)
 	var err error
+
 	estateCache.Range(func(_, v interface{}) bool {
 		e := v.(*Estate)
+		all = append(all, *e)
+		return true
+	})
 
+	estates := make([]Estate, 0)
+	for _, e := range all {
 		if doorHeight != nil {
 			if doorHeight.Min != -1 && e.DoorHeight < doorHeight.Min {
-				return true
+				continue
 			}
 			if doorHeight.Max != -1 && e.DoorHeight >= doorHeight.Max {
-				return true
+				continue
 			}
 		}
 
 		if doorWidth != nil {
 			if doorWidth.Min != -1 && e.DoorWidth < doorWidth.Min {
-				return true
+				continue
 			}
 			if doorWidth.Max != -1 && e.DoorWidth >= doorWidth.Max {
-				return true
+				continue
 			}
 		}
 
 		if rent != nil {
 			if rent.Min != -1 && e.Rent < rent.Min {
-				return true
+				continue
 			}
 			if rent.Max != -1 && e.Rent >= rent.Max {
-				return true
+				continue
 			}
 		}
 
 		if len(features) > 0 {
+			var matched bool
 			for _, f := range features {
-				if !strings.Contains(e.Features, f) {
-					return true
+				if strings.Contains(e.Features, f) {
+					matched = true
+					break
 				}
+			}
+			if !matched {
+				break
 			}
 		}
 
-		estates = append(estates, *e)
-		return true
-	})
+		estates = append(estates, e)
+	}
+
 	left := page * perPage
 	right := left + perPage
 	total := len(estates)
 	if right > total {
 		right = total
 	}
-	fmt.Printf("total=%v [%v:%v]\n", total, left, right)
 
 	sort.Slice(estates, func(i, j int) bool {
 		if estates[i].Popularity == estates[j].Popularity {
@@ -87,5 +96,6 @@ func searchEstatesCache(ctx context.Context, doorHeight, doorWidth, rent *Range,
 		}
 		return estates[i].Popularity > estates[j].Popularity
 	})
+
 	return estates[left:right], total, err
 }
